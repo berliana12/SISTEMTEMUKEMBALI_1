2f9eab44546981bfc0ae8657faf786b79dcd8d8c
berliana12 <42678722+berliana12@users.noreply.github.com>
2020-09-01 00:31:08 +0700

    Add files via upload
    MATERI 1 SISTEM TEMU KEMBALI
    "HELLOWORLD"

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color: #66ff99;">
+
+<h1 align="center">Sistem Temu Kembali</h1>
+
+<?php
+echo "<h3 align=\"center\">HelloWorld!</h3>";
+echo "<br>";
+echo "<h4 align=\"center\">Terimakasih Pak Eri</h4>";
+echo "<h4 align=\"center\">Berliana Siwi Humandari - 17.01.53.0103</h4>";
+?>
+
+</body>
+</html>
